78c7072cbe02226141e4f05bca0b29731e1d12b5
James McIntyre <james.checks.his.email@gmail.com>
2016-01-13 12:18:28 -0800

    min comp

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ $( document ).ready(init);
 
 function init() {
     $('#addtransgroup').submit(addNewTrans);
-    $('table').on('click', '#delete', removeRow);
-    $("table").on('click', '#earntoggle', onlyEarnings);
-    $("table").on('click', '#spendtoggle', onlySpending);
+    $('table').on('click','#delete', removeRow);
+    $("#earntoggle").on('click', onlyEarnings);
+    $("#spendtoggle").on('click', onlySpending);
 }
 
   var newBalance = 0;
-  var $removedEarn = {};
-  var $removedSpend = {};
+  var toggleEarn = true;
+  var toggleSpend = true;
 
 
 
@@ -22,57 +22,85 @@ function addNewTrans(formdata) {
   var newTransSpend = $('#spend').is(':checked');
   var newTransDesc = $('#description').val();
   var newTransEarn = $('#earn').is(':checked');
-  var newTransDate = moment().format("MMM Do YY");
+  var newTransDate = moment().format("MMM Do, YYYY");
   $('#addtransgroup').trigger("reset");
 
+
+  if (newTransDollarAmnt.match(/[^$,.\d]/)) {
+    console.log('invalid');
+    return alert('You gave an invalid Dollar amount!');
+  }
 
   var $newRow = $('#template').clone().removeAttr('id')
 
   $newRow.children('.date').text(newTransDate);
   $newRow.children('.description').text(newTransDesc);
     if (newTransSpend) {
-      $newRow.children('.spend').text('-'+currencyAmnt).addClass('danger');
-      updateBalance(currencyAmnt, "neg");
+      var negAmnt = ('-'+currencyAmnt);
+      $newRow.children('.spend').text(negAmnt).addClass('danger');
+      updateBalance(negAmnt, "neg");
       $newRow.attr('data', 'neg');
     }
     else {
-      $newRow.children('.earn').text('+'+currencyAmnt).addClass('success');
-      updateBalance(currencyAmnt, "pos");
+      var posAmnt = ('+'+currencyAmnt);
+      console.log(posAmnt);
+      $newRow.children('.earn').text(posAmnt).addClass('success');
+      updateBalance(posAmnt, "pos");
       $newRow.attr('data', 'pos');
     }
   $('#transhist').append($newRow);
   $('#delete').on('click', removeRow);
-  // updateBalance();
 
 }
 
 
 function removeRow(){
+  var removeSpendVal = $(this).closest('tr').find('.spend').text();
+  var removeEarnVal = $(this).closest('tr').find('.earn').text();
+
+  if (removeSpendVal !== "") {
+    updateBalance(removeSpendVal, 'pos');
+  }
+  else {
+    updateBalance(removeEarnVal, 'neg');
+  }
   $(this).closest('tr').remove();
+
 }
 
 function onlyEarnings(){
-  if ($('tr[data="neg"]').length > 0){
-    $removedSpend = $('tr[data="neg"]').detach();
+  if (toggleEarn) {
+    $('tr[data="neg"]').css('display', 'none');
+    toggleEarn = false;
 }
   else {
-    $('#transhist').append($removedSpend);
+    $('tr[data="neg"]').css('display', 'table-row');
+    toggleEarn = true;
   }
-
 }
 
+
+
+
 function onlySpending(){
-  $removedEarn = $('tr[data="pos"]').detach();
+  if (toggleSpend) {
+    $('tr[data="pos"]').css('display', 'none');
+    toggleSpend = false;
+}
+  else {
+    $('tr[data="pos"]').css('display', 'table-row');
+    toggleSpend = true;
+  }
 }
 
 
 
 function updateBalance(amount, toggle){
   if (toggle === "pos"){
-    newBalance += parseInt(amount.substr(1));
+    newBalance += parseFloat(amount.substr(2));
   }
   else {
-    newBalance -= parseInt(amount.substr(1));
+    newBalance -= parseFloat(amount.substr(2));
   }
   $('.panel1 h2').text('Current Balance: '+('$' + parseFloat(newBalance, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString()));
 ;
